d9bdaa93c1915c49e728879bd1d19e9d9a0a61e6
Redgy Canos <redgycanos@gmail.com>
2018-02-25 09:43:34 -0800

    toMaze

---
+++ Assets/EndGameTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            ;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/playerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerControl : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+       // count = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+   
+}
